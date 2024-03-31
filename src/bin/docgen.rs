4e8aca8610bf2de0d9840e3dddedf0ebb3bcc7ad
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2024-03-30 18:17:06 -0700

    refactor: remove toml

--- src/bin/docgen.rs
+++ src/bin/docgen.rs
@@ -3,7 +3,7 @@ use openpid::prelude::*;
 use openpid_docgen::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let spec: OpenPID = toml::from_str(&std::fs::read_to_string("./openpid.toml")?)?;
+    let spec = OpenPID::from_str(&std::fs::read_to_string("./openpid.toml")?)?;
     document(&spec, std::path::PathBuf::from("./outputs"))?;
     Ok(())
 }
